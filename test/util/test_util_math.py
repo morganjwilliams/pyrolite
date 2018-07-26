a2da1f3c53d736be19f7c3d5db8baf1941f56d11
Morgan Williams <morgan.j.williams@hotmail.com>
2018-07-26 17:08:31 +0800

    Add util.math lambda tests

--- test/util/test_util_math.py
+++ test/util/test_util_math.py
@@ -2,6 +2,7 @@ import unittest
 import pandas as pd
 import numpy as np
 from pyrolite.util.math import *
+from pyrolite.geochem import REE, get_radii
 
 
 class TestOnFinite(unittest.TestCase):
@@ -24,6 +25,32 @@ class TestOnFinite(unittest.TestCase):
             with self.subTest(f=f):
                 result = on_finite(arr, f)
                 self.assertTrue(np.isclose(result, f(arr[:2])))
+
+
+class TestOPConstants(unittest.TestCase):
+
+    def setUp(self):
+        self.xs = np.array(get_radii(REE()))
+        self.default_degree = 5
+
+    def test_xs(self):
+        for xs in [self.xs, self.xs[1:], self.xs[2:-2]]:
+            with self.subTest(xs=xs):
+                ret = OP_constants(xs, degree=self.default_degree)
+                self.assertTrue(not len(ret[0])) # first item is empty
+                self.assertTrue(len(ret) == self.default_degree)
+
+    def test_degree(self):
+        for degree in range(1, 6):
+            with self.subTest(degree=degree):
+                ret = OP_constants(self.xs, degree=degree)
+                self.assertTrue(not len(ret[0])) # first item is empty
+                self.assertTrue(len(ret) == degree)
+
+    def test_tol(self):
+        pass
+
+        #self.assertTrue(np.allclose(ret, X, atol=tol)
 
 
 if __name__ == '__main__':
