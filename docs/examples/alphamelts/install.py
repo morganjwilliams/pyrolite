c0347d61260d585e1606b2c6d88dbadc1f563ccd
Morgan Williams <morgan.j.williams@hotmail.com>
2019-04-09 12:56:52 +0800

    Add alphamelts local install info

---
+++ docs/examples/alphamelts/install.py
@@ -0,0 +1,7 @@
+from pyrolite.util.alphamelts.download import install_melts
+from pyrolite.util.meta import pyrolite_datafolder, stream_log
+
+# Here we can do a conditonal install - only downloading alphamelts if it doesnt exist
+if not (pyrolite_datafolder(subfolder="alphamelts") / "localinstall").exists():
+    stream_log("pyrolite.util.alphamelts", level="INFO")  # logger for output info
+    install_melts(local=True)  # install a copy of melts to pyrolite data folder
