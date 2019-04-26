84839ccc6182dbf1867f90b94a7d144d318144c3
Morgan Williams <morgan.j.williams@hotmail.com>
2019-04-26 16:03:44 +0800

    Remove unnecessary perl install from travis, notebook reqs from setup.py

--- setup.py
+++ setup.py
@@ -7,13 +7,10 @@ dev_require = [
     "pytest",
     "versioneer",
     "black",
-    "nbstripout",
-    "nbdime",
     "twine",
     "sphinx_rtd_theme",
     "sphinx-autodoc-annotation",
     "recommonmark",
-    #'mock',
 ] + tests_require
 
 db_require = ["pyodbc", "psycopg2"]
