8da9c9478c638b5875efb3aa599bb1fa76ef7312
Morgan Williams <morgan.j.williams@hotmail.com>
2019-05-01 11:18:13 +0800

    Update mineral.lattice docstrings
    Update mineral.lattice docstrings

--- pyrolite/mineral/lattice.py
+++ pyrolite/mineral/lattice.py
@@ -1,3 +1,32 @@
+"""
+Submodule for calcuating relative ion paritioning based on the lattice strain model [#ref_1]_ [#ref_2]_ [#ref_3]_.
+
+Todo
+------
+
+    * Bulk modulus and Youngs modulus approximations [#ref_3]_ [#ref_4]_  [#ref_5]_.
+
+References
+    ----------
+    .. [#ref_1] Brice, J.C., 1975. Some thermodynamic aspects of the growth of strained crystals.
+            Journal of Crystal Growth 28, 249–253.
+            doi: {brice1975}
+    .. [#ref_2] Blundy, J., Wood, B., 1994. Prediction of crystal–melt partition coefficients from elastic moduli.
+            Nature 372, 452.
+            doi: {blundy1994}
+    .. [#ref_3] Wood, B.J., Blundy, J.D., 2014. Trace Element Partitioning: The Influences of Ionic
+            Radius, Cation Charge, Pressure, and Temperature. Treatise on Geochemistry
+            (Second Edition) 3, 421–448.
+            doi: {wood2014}
+    .. [#ref_4] Anderson, D.L., Anderson, O.L., 1970.
+            Brief report: The bulk modulus-volume relationship for oxides.
+            Journal of Geophysical Research (1896-1977) 75, 3494–3500.
+            doi: {anderson1970}
+    .. [#ref_5] Hazen, R.M., Finger, L.W., 1979. Bulk modulus—volume relationship for cation-anion polyhedra.
+            Journal of Geophysical Research: Solid Earth 84, 6723–6728.
+            doi: {hazen1979}
+
+"""
 import numpy as np
 from ..util.meta import sphinx_doi_link, update_docstring_references
 
@@ -5,7 +34,7 @@ from ..util.meta import sphinx_doi_link, update_docstring_references
 @update_docstring_references
 def strain_coefficient(r0, rx, E=None, T=298.15, **kwargs):
     r"""
-    Calculate the lattice strain associated with an ionic substitution [#ref_1]_  [#ref_2]_  [#ref_3]_  [#ref_4]_.
+    Calculate the lattice strain associated with an ionic substitution [#ref_1]_ [#ref_2]_.
 
     Parameters
     -----------
@@ -66,6 +95,14 @@ def strain_coefficient(r0, rx, E=None, T=298.15, **kwargs):
     rterm = (r0 / 2) * (rx - r0) ** 2 - (1 / 3) * (rx - r0) ** 3
     return np.exp(coeff * rterm * (-n / (8.314 * T)))
 
+__doc__ = __doc__.format(
+    brice1975=sphinx_doi_link("10.1016/0022-0248(75)90241-9"),
+    blundy1994=sphinx_doi_link("10.1038/372452a0"),
+    anderson1970=sphinx_doi_link("10.1029/JB075i017p03494"),
+    hazen1979=sphinx_doi_link("10.1029/JB084iB12p06723"),
+    wood2014=sphinx_doi_link("10.1016/B978-0-08-095975-7.00209-6")
+)
+__doc__ = str(__doc__).replace("ref", __name__)
 
 # add references for the lattice strain model, needs to be done here due to nested {}
 # in the math blocks
@@ -77,19 +114,9 @@ sc_ref = r"""References
     .. [#ref_2] Blundy, J., Wood, B., 1994. Prediction of crystal–melt partition coefficients from elastic moduli.
             Nature 372, 452.
             doi: {blundy1994}
-    .. [#ref_3] Blundy, J., Wood, B., 2003. Partitioning of trace elements between crystals and melts.
-            Earth and Planetary Science Letters 210, 383–397.
-            doi: {blundy2003}
-    .. [#ref_4] Wood, B.J., Blundy, J.D., 2014. Trace Element Partitioning: The Influences of Ionic
-            Radius, Cation Charge, Pressure, and Temperature. Treatise on Geochemistry
-            (Second Edition) 3, 421–448.
-            doi: {wood2014}
-
     """.format(
         brice1975=sphinx_doi_link("10.1016/0022-0248(75)90241-9"),
-        blundy1994=sphinx_doi_link("10.1038/372452a0"),
-        blundy2003=sphinx_doi_link("10.1016/S0012-821X(03)00129-8"),
-        wood2014=sphinx_doi_link("10.1016/B978-0-08-095975-7.00209-6"),
+        blundy1994=sphinx_doi_link("10.1038/372452a0")
 )
 sc_ref = sc_ref.replace("ref", strain_coefficient.__name__)
 strain_coefficient.__doc__ = strain_coefficient.__doc__ + sc_ref
