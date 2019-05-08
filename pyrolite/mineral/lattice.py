f49030fb1f01f2ad7faab019a8e31e8c8516af4b
Morgan Williams <morgan.j.williams@hotmail.com>
2019-05-09 07:47:08 +1000

    Add Youngs modulus approximation

--- pyrolite/mineral/lattice.py
+++ pyrolite/mineral/lattice.py
@@ -32,7 +32,7 @@ from ..util.meta import sphinx_doi_link, update_docstring_references
 
 
 @update_docstring_references
-def strain_coefficient(r0, rx, E=None, T=298.15, **kwargs):
+def strain_coefficient(r0, rx, E=None, T=298.15, z=None, **kwargs):
     r"""
     Calculate the lattice strain associated with an ionic substitution [#ref_1]_ [#ref_2]_.
 
@@ -46,6 +46,10 @@ def strain_coefficient(r0, rx, E=None, T=298.15, **kwargs):
         Young's modulus (stiffness) for the site, in pascals (Pa).
     T : :class:`float`
         Temperature, in Kelvin (K).
+    z : :class:`int`
+        Optional specification of cationic valence, for calcuation of approximate
+        Young's modulus using :func:`youngs_modulus_approximation`,
+        where the modulus is not specified.
 
     Returns
     --------
@@ -89,18 +93,84 @@ def strain_coefficient(r0, rx, E=None, T=298.15, **kwargs):
         The model assumes that the crystal is elastically isotropic.
 
     """
+    E = E or youngs_modulus_approximation(z, r0) # use E if defined, else try to calc
     n = 6.023 * 10 ** 23
     r0, rx = r0 / 10 ** 10, rx / 10 ** 10  # convert to meters
     coeff = 4 * np.pi * E
     rterm = (r0 / 2) * (rx - r0) ** 2 - (1 / 3) * (rx - r0) ** 3
     return np.exp(coeff * rterm * (-n / (8.314 * T)))
 
+
+@update_docstring_references
+def youngs_modulus_approximation(z, r):
+    r"""
+    Young's modulus approximation for cationic sites in silicates
+    and oxides [#ref_1]_ [#ref_2]_ [#ref_3]_.
+
+    Parameters
+    ----------
+    z : :class:`integer`
+        Cationic valence.
+    r : :class:`float`
+        Ionic radius of the cation (Å).
+
+    Returns
+    --------
+    E : :class:`float`
+        Young's modulus for the cationic site, in Pascals (Pa)
+    Notes
+    ------
+    The bulk modulus :math:`K` for an an ionic crystal is esimated using [#ref_1]_:
+
+    .. math::
+
+        K = \frac{A Z_a Z_c e^2 (n-1)}{9 d_0 V_0}
+
+    Where :math:`A` is the Madelung constant, :math:`Z_c` and :math:`Z_a` are the
+    anion and cation valences, :math:`e` is the charge on the electron,
+    :math:`n` is the Born power law coefficent, and :math:`d_0` is the cation-anion
+    distance [#ref_1]_. Using the Shannon ionic radius for oxygen (1.38 Å),
+    this is approximated for cations coordinated by oxygen in silicates and oxides
+    using the following relationship [#ref_2]_:
+
+    .. math::
+
+        K = 750 Z_c d^{-3}
+
+    Where :math:`d` is the cation-anion distance (Å), :math:`Z_c` is the
+    cationic valence (and :math:`K` is in GPa). The Young's modulus :math:`E` is
+    then calculated through the relationship [#ref_3]_:
+
+    .. math::
+
+        E = 3 K (1 - 2 \sigma)
+
+    Where :math:`\sigma` is Poisson's ratio, which in the case of minerals can be
+    approimxated by 0.25 [#ref_3]_, and hence:
+
+    .. math::
+
+        \begin{align}
+            E &\approx 1.5 K\\
+            E &\approx 1025 Z_c d^{-3}
+        \end{align}
+
+    Todo
+    -----
+
+        * Add links to docstring
+
+    """
+    d = r + 1.38
+    E = 1.5 * 750 * (z / d ** 3) * 10 ** 9
+    return E
+
 __doc__ = __doc__.format(
     brice1975=sphinx_doi_link("10.1016/0022-0248(75)90241-9"),
     blundy1994=sphinx_doi_link("10.1038/372452a0"),
     anderson1970=sphinx_doi_link("10.1029/JB075i017p03494"),
     hazen1979=sphinx_doi_link("10.1029/JB084iB12p06723"),
-    wood2014=sphinx_doi_link("10.1016/B978-0-08-095975-7.00209-6")
+    wood2014=sphinx_doi_link("10.1016/B978-0-08-095975-7.00209-6"),
 )
 __doc__ = str(__doc__).replace("ref", __name__)
 
@@ -115,8 +185,30 @@ sc_ref = r"""References
             Nature 372, 452.
             doi: {blundy1994}
     """.format(
-        brice1975=sphinx_doi_link("10.1016/0022-0248(75)90241-9"),
-        blundy1994=sphinx_doi_link("10.1038/372452a0")
+    brice1975=sphinx_doi_link("10.1016/0022-0248(75)90241-9"),
+    blundy1994=sphinx_doi_link("10.1038/372452a0"),
 )
 sc_ref = sc_ref.replace("ref", strain_coefficient.__name__)
 strain_coefficient.__doc__ = strain_coefficient.__doc__ + sc_ref
+
+bm_ref = r"""References
+    -----------
+        .. [#ref_1] Anderson, D.L., Anderson, O.L., 1970.
+                Brief report: The bulk modulus-volume relationship for oxides.
+                Journal of Geophysical Research (1896-1977) 75, 3494–3500.
+                doi: {anderson1970}
+        .. [#ref_2] Hazen, R.M., Finger, L.W., 1979. Bulk modulus—volume relationship for
+                cation-anion polyhedra. Journal of Geophysical Research: Solid Earth 84,
+                6723–6728.
+                doi: {hazen1979}
+        .. [#ref_3] Wood, B.J., Blundy, J.D., 2014. Trace Element Partitioning: The Influences of Ionic
+                Radius, Cation Charge, Pressure, and Temperature. Treatise on Geochemistry
+                (Second Edition) 3, 421–448.
+                doi: {wood2014}
+    """.format(
+    anderson1970=sphinx_doi_link("10.1029/JB075i017p03494"),
+    hazen1979=sphinx_doi_link("10.1029/JB084iB12p06723"),
+    wood2014=sphinx_doi_link("10.1016/B978-0-08-095975-7.00209-6"),
+)
+bm_ref = bm_ref.replace("ref", youngs_modulus_approximation.__name__)
+youngs_modulus_approximation.__doc__ = youngs_modulus_approximation.__doc__ + bm_ref
