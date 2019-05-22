0d89cf081462edc9fd309d6d68ba2e3ae05d59d8
Morgan Williams <morgan.j.williams@hotmail.com>
2019-05-22 16:12:11 +0800

    Update pyroplot interface, add convenience pyroplot.scatter

--- pyrolite/plot/__init__.py
+++ pyrolite/plot/__init__.py
@@ -45,6 +45,34 @@ class pyroplot(object):
     def _validate(obj):
         pass
 
+    def cooccurence(self, ax=None, normalize=True, log=False, colorbar=False, **kwargs):
+        """
+        Plot the co-occurence frequency matrix for a given input.
+
+        Parameters
+        -----------
+        ax : :class:`matplotlib.axes.Axes`, :code:`None`
+            The subplot to draw on.
+        normalize : :class:`bool`
+            Whether to normalize the cooccurence to compare disparate variables.
+        log : :class:`bool`
+            Whether to take the log of the cooccurence.
+        colorbar : :class:`bool`
+            Whether to append a colorbar.
+
+        Returns
+        --------
+        :class:`matplotlib.axes.Axes`
+            Axes on which the cooccurence plot is added.
+        """
+        obj = to_frame(self._obj)
+        ax = plot_cooccurence(
+            obj.values, ax=ax, normalize=normalize, log=log, colorbar=colorbar, **kwargs
+        )
+        ax.set_xticklabels(obj.columns, minor=False, rotation=90)
+        ax.set_yticklabels(obj.columns, minor=False)
+        return ax
+
     def density(self, components: list = None, ax=None, axlabels=True, **kwargs):
         r"""
         Method for plotting histograms (mode='hist2d'|'hexbin') or kernel density
@@ -96,6 +124,189 @@ class pyroplot(object):
                 tax.bottom_axis_label(components[2], fontsize=fontsize)
         else:
             pass
+
+        return ax
+
+    def REE(self, index="radii", ax=None, mode="plot", **kwargs):
+        """Pass the pandas object to :func:`pyrolite.plot.spider.REE_v_radii`.
+
+        Parameters
+        ------------
+        ax : :class:`matplotlib.axes.Axes`, :code:`None`
+            The subplot to draw on.
+        index : :class:`str`
+            Whether to plot radii ('radii') on the principal x-axis, or elements
+            ('elements').
+        mode : :class:`str`, :code`["plot", "fill", "binkde", "ckde", "kde", "hist"]`
+            Mode for plot. Plot will produce a line-scatter diagram. Fill will return
+            a filled range. Density will return a conditional density diagram.
+
+        {otherparams}
+
+        Returns
+        -------
+        :class:`matplotlib.axes.Axes`
+            Axes on which the REE plot is added.
+        """
+        obj = to_frame(self._obj)
+        ree = REE()
+
+        ax = spider.REE_v_radii(
+            obj.loc[:, ree].values, ree=ree, mode=mode, ax=ax, **kwargs
+        )
+        ax.set_ylabel(" $\mathrm{X / X_{Reference}}$")
+        return ax
+
+    def scatter(self, components: list = None, ax=None, **kwargs):
+        r"""
+        Convenience method for scatter plots using the pyroplot API. See
+        further parameters for `matplotlib.pyplot.scatter` function below.
+
+        Parameters
+        -----------
+        components : :class:`list`, `None`
+            Elements or compositional components to plot.
+        ax : :class:`matplotlib.axes.Axes`, :code:`None`
+            The subplot to draw on.
+
+        {otherparams}
+
+        Returns
+        -------
+        :class:`matplotlib.axes.Axes`
+            Axes on which the scatterplot is added.
+        """
+        obj = to_frame(self._obj)
+        try:
+            if obj.columns.size not in [2, 3]:
+                assert len(components) in [2, 3]
+
+            if components is None:
+                components = obj.columns.values
+        except:
+            msg = "Suggest components or provide a slice of the dataframe."
+            raise AssertionError(msg)
+
+        fontsize = kwargs.get("fontsize", 8.0)
+
+        if len(components) == 3:
+            ax = obj.loc[:, components].pyroplot.ternary(ax=ax, **kwargs)
+        else:  # len(components) == 2
+            xvar, yvar = components
+            ax = plt.scatter(
+                obj.loc[:, xvar].values, obj.loc[:, yvar].values, ax=ax, **kwargs
+            )
+            if axlabels:
+                ax.set_xlabel(xvar, fontsize=fontsize)
+                ax.set_ylabel(yvar, fontsize=fontsize)
+
+        return ax
+
+    def spider(
+        self,
+        components: list = None,
+        indexes: list = None,
+        ax=None,
+        mode="plot",
+        **kwargs
+    ):
+        r"""
+        Method for spider plots. Convenience access function to
+        :func:`~pyrolite.plot.spider.spider` (see `Other Parameters`, below), where
+        further parameters for relevant `matplotlib` functions are also listed.
+
+        Parameters
+        -----------
+        components : :class:`list`, `None`
+            Elements or compositional components to plot.
+        indexes :  :class:`list`, `None`
+            Elements or compositional components to plot.
+        ax : :class:`matplotlib.axes.Axes`, :code:`None`
+            The subplot to draw on.
+        mode : :class:`str`, :code`["plot", "fill", "binkde", "ckde", "kde", "hist"]`
+            Mode for plot. Plot will produce a line-scatter diagram. Fill will return
+            a filled range. Density will return a conditional density diagram.
+
+        {otherparams}
+
+        Returns
+        -------
+        :class:`matplotlib.axes.Axes`
+            Axes on which the spider diagram is plotted.
+
+        Todo
+        -----
+            * Add 'compositional data' filter for default components if None is given
+        """
+        obj = to_frame(self._obj)
+
+        if components is None:  # default to plotting elemental data
+            components = [el for el in obj.columns if el in common_elements()]
+
+        assert len(components) != 0
+
+        ax = spider.spider(
+            obj.loc[:, components].values, indexes=indexes, ax=ax, mode=mode, **kwargs
+        )
+        ax.set_xticklabels(components, rotation=60)
+        return ax
+
+    def stem(
+        self,
+        components: list = None,
+        ax=None,
+        orientation="horizontal",
+        axlabels=True,
+        **kwargs
+    ):
+        r"""
+        Method for creating stem plots. Convenience access function to
+        :func:`~pyrolite.plot.stem.stem` (see `Other Parameters`, below), where
+        further parameters for relevant `matplotlib` functions are also listed.
+
+        Parameters
+        -----------
+        components : :class:`list`, :code:`None`
+            Elements or compositional components to plot.
+        ax : :class:`matplotlib.axes.Axes`, :code:`None`
+            The subplot to draw on.
+        orientation : :class:`str`
+            Orientation of the plot (horizontal or vertical).
+        axlabels : :class:`bool`, True
+            Whether to add x-y axis labels.
+
+        Other Parameters
+        ------------------
+        {otherparams}
+
+        Returns
+        -------
+        :class:`matplotlib.axes.Axes`
+            Axes on which the stem diagram is plotted.
+        """
+        obj = to_frame(self._obj)
+        try:
+            if obj.columns.size not in [2, 3]:
+                assert len(components) in [2, 3]
+
+            if components is None:
+                components = obj.columns.values
+        except:
+            msg = "Suggest components or provide a slice of the dataframe."
+            raise AssertionError(msg)
+        ax = stem.stem(
+            obj[components[0]].values,
+            obj[components[1]].values,
+            ax=ax,
+            orientation=orientation,
+            **kwargs
+        )
+
+        if axlabels:
+            if "h" not in orientation:
+                components = components[::-1]
+            ax.set_xlabel(components[0])
+            ax.set_ylabel(components[1])
 
         return ax
 
@@ -158,172 +369,6 @@ class pyroplot(object):
         ax.set_aspect("equal")
         return ax
 
-    def spider(
-        self,
-        components: list = None,
-        indexes: list = None,
-        ax=None,
-        mode="plot",
-        **kwargs
-    ):
-        r"""
-        Method for spider plots. Convenience access function to
-        :func:`~pyrolite.plot.spider.spider` (see `Other Parameters`, below), where
-        further parameters for relevant `matplotlib` functions are also listed.
-
-        Parameters
-        -----------
-        components : :class:`list`, `None`
-            Elements or compositional components to plot.
-        indexes :  :class:`list`, `None`
-            Elements or compositional components to plot.
-        ax : :class:`matplotlib.axes.Axes`, :code:`None`
-            The subplot to draw on.
-        mode : :class:`str`, :code`["plot", "fill", "binkde", "ckde", "kde", "hist"]`
-            Mode for plot. Plot will produce a line-scatter diagram. Fill will return
-            a filled range. Density will return a conditional density diagram.
-
-        {otherparams}
-
-        Returns
-        -------
-        :class:`matplotlib.axes.Axes`
-            Axes on which the spider diagram is plotted.
-
-        Todo
-        -----
-            * Add 'compositional data' filter for default components if None is given
-        """
-        obj = to_frame(self._obj)
-
-        if components is None:  # default to plotting elemental data
-            components = [el for el in obj.columns if el in common_elements()]
-
-        assert len(components) != 0
-
-        ax = spider.spider(
-            obj.loc[:, components].values, indexes=indexes, ax=ax, mode=mode, **kwargs
-        )
-        ax.set_xticklabels(components, rotation=60)
-        return ax
-
-    def REE(self, index="radii", ax=None, mode="plot", **kwargs):
-        """Pass the pandas object to :func:`pyrolite.plot.spider.REE_v_radii`.
-
-        Parameters
-        ------------
-        ax : :class:`matplotlib.axes.Axes`, :code:`None`
-            The subplot to draw on.
-        index : :class:`str`
-            Whether to plot radii ('radii') on the principal x-axis, or elements
-            ('elements').
-        mode : :class:`str`, :code`["plot", "fill", "binkde", "ckde", "kde", "hist"]`
-            Mode for plot. Plot will produce a line-scatter diagram. Fill will return
-            a filled range. Density will return a conditional density diagram.
-
-        {otherparams}
-
-        Returns
-        -------
-        :class:`matplotlib.axes.Axes`
-            Axes on which the REE plot is added.
-        """
-        obj = to_frame(self._obj)
-        ree = REE()
-
-        ax = spider.REE_v_radii(
-            obj.loc[:, ree].values, ree=ree, mode=mode, ax=ax, **kwargs
-        )
-        ax.set_ylabel(" $\mathrm{X / X_{Reference}}$")
-        return ax
-
-    def stem(
-        self,
-        components: list = None,
-        ax=None,
-        orientation="horizontal",
-        axlabels=True,
-        **kwargs
-    ):
-        r"""
-        Method for creating stem plots. Convenience access function to
-        :func:`~pyrolite.plot.stem.stem` (see `Other Parameters`, below), where
-        further parameters for relevant `matplotlib` functions are also listed.
-
-        Parameters
-        -----------
-        components : :class:`list`, :code:`None`
-            Elements or compositional components to plot.
-        ax : :class:`matplotlib.axes.Axes`, :code:`None`
-            The subplot to draw on.
-        orientation : :class:`str`
-            Orientation of the plot (horizontal or vertical).
-        axlabels : :class:`bool`, True
-            Whether to add x-y axis labels.
-
-        Other Parameters
-        ------------------
-        {otherparams}
-
-        Returns
-        -------
-        :class:`matplotlib.axes.Axes`
-            Axes on which the stem diagram is plotted.
-        """
-        obj = to_frame(self._obj)
-        try:
-            if obj.columns.size not in [2, 3]:
-                assert len(components) in [2, 3]
-
-            if components is None:
-                components = obj.columns.values
-        except:
-            msg = "Suggest components or provide a slice of the dataframe."
-            raise AssertionError(msg)
-        ax = stem.stem(
-            obj[components[0]].values,
-            obj[components[1]].values,
-            ax=ax,
-            orientation=orientation,
-            **kwargs
-        )
-
-        if axlabels:
-            if 'h' not in  orientation:
-                components = components[::-1]
-            ax.set_xlabel(components[0])
-            ax.set_ylabel(components[1])
-
-        return ax
-
-    def cooccurence(self, ax=None, normalize=True, log=False, colorbar=False, **kwargs):
-        """
-        Plot the co-occurence frequency matrix for a given input.
-
-        Parameters
-        -----------
-        ax : :class:`matplotlib.axes.Axes`, :code:`None`
-            The subplot to draw on.
-        normalize : :class:`bool`
-            Whether to normalize the cooccurence to compare disparate variables.
-        log : :class:`bool`
-            Whether to take the log of the cooccurence.
-        colorbar : :class:`bool`
-            Whether to append a colorbar.
-
-        Returns
-        --------
-        :class:`matplotlib.axes.Axes`
-            Axes on which the cooccurence plot is added.
-        """
-        obj = to_frame(self._obj)
-        ax = plot_cooccurence(
-            obj.values, ax=ax, normalize=normalize, log=log, colorbar=colorbar, **kwargs
-        )
-        ax.set_xticklabels(obj.columns, minor=False, rotation=90)
-        ax.set_yticklabels(obj.columns, minor=False)
-        return ax
-
 
 # ideally we would i) check for the same params and ii) aggregate all others across
 # inherited or chained functions. This simply imports the params from another docstring
@@ -342,12 +387,27 @@ pyroplot.density.__doc__ = pyroplot.density.__doc__.format(
     ][_add_additional_parameters]
 )
 
-pyroplot.ternary.__doc__ = pyroplot.ternary.__doc__.format(
+
+pyroplot.REE.__doc__ = pyroplot.REE.__doc__.format(
     otherparams=[
         "",
         get_additional_params(
-            pyroplot.ternary,
-            tern.ternary,
+            pyroplot.REE,
+            spider.REE_v_radii,
+            header="Other Parameters",
+            indent=8,
+            subsections=True,
+        ),
+    ][_add_additional_parameters]
+)
+
+
+pyroplot.scatter.__doc__ = pyroplot.scatter.__doc__.format(
+    otherparams=[
+        "",
+        get_additional_params(
+            pyroplot.scatter,
+            plt.scatter,
             header="Other Parameters",
             indent=8,
             subsections=True,
@@ -368,18 +428,6 @@ pyroplot.spider.__doc__ = pyroplot.spider.__doc__.format(
     ][_add_additional_parameters]
 )
 
-pyroplot.REE.__doc__ = pyroplot.REE.__doc__.format(
-    otherparams=[
-        "",
-        get_additional_params(
-            pyroplot.REE,
-            spider.REE_v_radii,
-            header="Other Parameters",
-            indent=8,
-            subsections=True,
-        ),
-    ][_add_additional_parameters]
-)
 
 pyroplot.stem.__doc__ = pyroplot.stem.__doc__.format(
     otherparams=[
@@ -387,6 +435,19 @@ pyroplot.stem.__doc__ = pyroplot.stem.__doc__.format(
         get_additional_params(
             pyroplot.stem,
             stem.stem,
+            header="Other Parameters",
+            indent=8,
+            subsections=True,
+        ),
+    ][_add_additional_parameters]
+)
+
+pyroplot.ternary.__doc__ = pyroplot.ternary.__doc__.format(
+    otherparams=[
+        "",
+        get_additional_params(
+            pyroplot.ternary,
+            tern.ternary,
             header="Other Parameters",
             indent=8,
             subsections=True,
