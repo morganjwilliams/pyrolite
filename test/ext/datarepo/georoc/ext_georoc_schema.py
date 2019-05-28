dc869a8accd0f64579d60a7a3637c6cd0467548c
Morgan Williams <morgan.j.williams@hotmail.com>
2019-05-28 10:54:03 +0800

    Modify georoc test

--- test/ext/datarepo/georoc/ext_georoc_schema.py
+++ test/ext/datarepo/georoc/ext_georoc_schema.py
@@ -1,8 +1,7 @@
 import unittest
 from pyrolite.util.web import internet_connection, download_file
 from pyrolite.geochem.parse import check_multiple_cation_inclusion
-from pyrolite.ext.datarepo.georoc.parse import georoc_munge
-from pyrolite.ext.datarepo.georoc.schema import parse_GEOROC_response
+from pyrolite.ext.datarepo.georoc.schema import parse_GEOROC_response, georoc_munge
 
 
 @unittest.skipIf(not internet_connection(), "Needs internet connection.")
