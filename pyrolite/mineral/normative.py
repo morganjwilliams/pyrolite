8234bdd3e67dcfed4960ceb5c5c676d5ffc2ce30
Morgan Williams <morgan.j.williams@hotmail.com>
2019-06-10 10:12:03 +0800

    Add mineral.normative.CIPW template

---
+++ pyrolite/mineral/normative.py
@@ -0,0 +1,91 @@
+
+import periodictable as pt
+
+
+def CIPW_norm(df, form="weight"):
+    """
+    Standardised calcuation of estimated mineralogy from bulk rock chemistry.
+    Takes a dataframe of chemistry and creates a dataframe of estimated mineralogy.
+
+    Parameters
+    -----------
+    df : :class:`pandas.DataFrame`
+        Dataframe containing compositions to transform.
+
+    Returns
+    --------
+    :class:`pandas.DataFrame`
+    """
+
+    # devolatilise and renormalise
+
+    # deal with Fe oxidation
+
+    # convert to molar data
+
+    # aggregate like-elements to their most commom member
+    # Ferric : FeO, MnO, NiO, CoO
+    # Calcic : CaO, BaO, SrO
+    # Potassic : K2O, Rb2O, Cs2O
+    # Sodic : Na2O, Li2O
+    # Chromic : Cr2O3, V2O3
+
+    # recalculate 'molecular weigths' for each of these components, and the minerals
+
+    # Zircon
+
+    # Apatite
+
+    # Flourite
+
+    # halite
+
+    # thenardite
+
+    # pyrite
+
+    # cancrinite or calcite
+
+    # chromite
+
+    # ilmenite
+
+    # orthoclase / potassium metasilicate
+
+    # albite
+
+    # acmite / sodium metasilicate
+
+    # anorthite / corundum
+
+    # titanite / rutile
+
+    # magnetite / hematite
+
+    # diopside / wollastonite / hypersthene
+
+    # quartz / undersadurated minerals
+
+    # silica deficiency?
+
+    # olivine / hypersthene
+
+    # titanite / perobskite
+
+    # nephenline / albite
+
+    # leucite / orthoclase
+
+    # dicalcium silicate / wollastonite
+
+    # dicalcium silicate / olivine adjustment
+
+    # kaliophilite / leucite
+
+    # allocate provisional mineral proportions to definite ones
+
+    # normalise
+
+    # calculate Salic, Femic, C.I., D.I
+
+    pass
