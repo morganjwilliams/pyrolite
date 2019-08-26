0e58dc2195a19248aa48a9db47630f5965d8c0ec
Morgan Williams <morgan.j.williams@hotmail.com>
2019-08-26 09:07:05 +0800

    Add pyrochem accessor

--- pyrolite/geochem/__init__.py
+++ pyrolite/geochem/__init__.py
@@ -2,6 +2,7 @@
 Submodule for working with geochemical data.
 """
 import logging
+import pandas as pd
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 logger = logging.getLogger(__name__)
@@ -14,3 +15,22 @@ from .transform import *
 from .validate import *
 from .alteration import *
 from .norm import *
+
+# note that only some of these methods will be valid for series
+@pd.api.extensions.register_series_accessor("pyrochem")
+@pd.api.extensions.register_dataframe_accessor("pyrochem")
+class pyrochem(object):
+    """
+    Custom dataframe accessor for pyrolite geochemistry.
+    """
+
+    def __init__(self, obj):
+        self._validate(obj)
+        self._obj = obj
+
+    @staticmethod
+    def _validate(obj):
+        pass
+
+    def norm_to(self, norm_to=None):
+        pass
