e3f8650e48046e19cdf76beb9ac676f705d4b41b
Morgan Williams <morgan.j.williams@hotmail.com>
2019-09-16 14:26:00 +0800

    Add note to parallel example

--- docs/examples/plotting/parallel.py
+++ docs/examples/plotting/parallel.py
@@ -19,7 +19,7 @@ from pyrolite.util.plot import save_figure
 save_figure(ax.figure, save_at="../../source/_static", name="parallel_default")
 # %% Default Rescale
 ax = df.pyroplot.parallel(rescale=True)
-# %%
+# %% save
 save_figure(ax.figure, save_at="../../source/_static", name="parallel_rescale")
 # %% CLR
 from pyrolite.util.skl.transform import CLRTransform
@@ -28,6 +28,7 @@ compdata = df.copy()
 compdata[comp] = CLRTransform().transform(compdata[comp])
 ax = compdata.loc[:, comp].pyroplot.parallel(color_by=compdata.Depth.values)
 
+# we can add a meaningful colorbar to indicate one variable also, here Depth
 sm = plt.cm.ScalarMappable(cmap="viridis")
 sm.set_array(df.Depth)
 plt.colorbar(sm)
