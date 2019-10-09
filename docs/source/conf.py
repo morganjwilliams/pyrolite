6ba6848a49a0836b57bda8d4e3ffcb8ae0d1f535
Morgan Williams <morgan.j.williams@hotmail.com>
2019-10-09 10:02:34 +0800

    Update docs config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -115,7 +115,11 @@ html_theme = "sphinx_rtd_theme"
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-html_theme_options = {"logo_only": True, "prev_next_buttons_location": None}
+html_theme_options = {
+    "logo_only": True,
+    "prev_next_buttons_location": None,
+    "vcs_pageview_mode": "edit",
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
