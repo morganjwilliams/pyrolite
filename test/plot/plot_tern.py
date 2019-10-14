0bc0d55936f5132332fbf45683f7106ed8d3b45c
Morgan Williams <morgan.j.williams@hotmail.com>
2019-10-14 12:59:02 +0800

    Fill test Stubs for plot.tern

--- test/plot/plot_tern.py
+++ test/plot/plot_tern.py
@@ -5,6 +5,8 @@ import numpy as np
 import logging
 import ternary as pyternary
 from pyrolite.plot.tern import ternary
+import matplotlib.colors
+
 
 logger = logging.getLogger(__name__)
 
@@ -23,7 +25,6 @@ class TestTernaryplot(unittest.TestCase):
         self.assertEqual(
             type(out.tax), pyternary.ternary_axes_subplot.TernaryAxesSubplot
         )
-        plt.close("all")
 
     def test_one(self):
         """Test generation of plot with one record."""
@@ -33,7 +34,6 @@ class TestTernaryplot(unittest.TestCase):
         self.assertEqual(
             type(out.tax), pyternary.ternary_axes_subplot.TernaryAxesSubplot
         )
-        plt.close("all")
 
     def test_multiple(self):
         """Test generation of plot with multiple records."""
@@ -43,27 +43,63 @@ class TestTernaryplot(unittest.TestCase):
         self.assertEqual(
             type(out.tax), pyternary.ternary_axes_subplot.TernaryAxesSubplot
         )
-        plt.close("all")
 
     def test_overplotting(self):
         """Test use of the plot for multiple rounds of plotting."""
-        pass
+        arr = self.arr
+        out = ternary(arr)
+        self.assertTrue(hasattr(out, "tax"))
+        out2 = ternary(arr, ax=out)
+        self.assertTrue(out.tax is out2.tax) # hasn't added a new ternary axis
 
     def test_color_tuple(self):
-        pass
+        arr = self.arr
+        out = ternary(arr, c=(0.1, 0.2, 0.5, 0.3))
+        self.assertTrue(hasattr(out, "tax"))
+        self.assertEqual(
+            type(out.tax), pyternary.ternary_axes_subplot.TernaryAxesSubplot
+        )
+
+    def test_color_hex(self):
+        arr = self.arr
+        out = ternary(arr, c="#0f0f0f")
+        self.assertTrue(hasattr(out, "tax"))
+        self.assertEqual(
+            type(out.tax), pyternary.ternary_axes_subplot.TernaryAxesSubplot
+        )
 
     def test_color_cmap_c_combination(self):
         """
         Check than array of values specified using `c`
         can be used for a colormap.
         """
-        pass
+        arr = self.arr
+        out = ternary(arr, c=np.linspace(0, 10, 10), cmap="viridis")
+        self.assertTrue(hasattr(out, "tax"))
+        self.assertEqual(
+            type(out.tax), pyternary.ternary_axes_subplot.TernaryAxesSubplot
+        )
 
     def test_norm_specified(self):
-        pass
+        arr = self.arr
+        out = ternary(
+            arr,
+            c=np.random.randn(10),
+            cmap="viridis",
+            norm=matplotlib.colors.Normalize(vmin=-1.0, vmax=1.0),
+        )
+        self.assertTrue(hasattr(out, "tax"))
+        self.assertEqual(
+            type(out.tax), pyternary.ternary_axes_subplot.TernaryAxesSubplot
+        )
 
     def test_label_specified(self):
-        pass
+        arr = self.arr
+        out = ternary(arr, label="testarr")
+        self.assertTrue(hasattr(out, "tax"))
+        self.assertEqual(
+            type(out.tax), pyternary.ternary_axes_subplot.TernaryAxesSubplot
+        )
 
     def tearDown(self):
         plt.close("all")
