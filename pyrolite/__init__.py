d06a1ffd179a5a7e4c4ea9fbd5d3aa18e45d8640
Morgan Williams <morgan.j.williams@hotmail.com>
2019-10-31 08:56:17 +0800

    Add extension loader

--- pyrolite/__init__.py
+++ pyrolite/__init__.py
@@ -1,5 +1,7 @@
 import sys
 import logging
+import importlib
+import pkgutil
 
 # http://docs.python-guide.org/en/latest/writing/logging/
 logging.getLogger(__name__).addHandler(logging.NullHandler())
@@ -9,3 +11,32 @@ from ._version import get_versions
 
 __version__ = get_versions()["version"]
 del get_versions
+
+
+def load_extensions(base="pyrolite_", replace=["util"]):
+    """
+    Automatically load any extensions associated with pyrolite
+    to be importable from :mod:`pyrolite.extensions`.
+
+    Parameters
+    ------------
+    base : :class:`str`
+        Module base string pattern for recognising extensions.
+    replace : :class:`list`
+        List of strings to replace from extension modules to shorten call signatures.
+    """
+    from . import extensions
+
+    modules = {
+        name.replace(base, ""): importlib.import_module(name)
+        for finder, name, ispkg in pkgutil.iter_modules()
+        if name.startswith(base)
+    }
+    output = {}
+    for n, m in modules.items():
+        for r in replace:
+            n = n.replace(r, "")
+        setattr(extensions, n, m)
+
+
+load_extensions()
