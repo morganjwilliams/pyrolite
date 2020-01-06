8a950e4d68a650cf0017615f7136f5b8eb3d1fa7
Morgan Williams <morgan.j.williams@hotmail.com>
2020-01-06 15:36:01 +0800

    Remove 3.5 support

--- setup.py
+++ setup.py
@@ -42,7 +42,6 @@ setup(
         "Intended Audience :: Science/Research",
         "Natural Language :: English",
         "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
