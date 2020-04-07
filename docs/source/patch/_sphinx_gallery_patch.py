6dae81b66c821f25068d9dae6ee855066a210219
Morgan Williams <morgan.j.williams@hotmail.com>
2020-04-07 14:07:50 +0800

    Add local docs_resolv to patch

--- docs/source/patch/_sphinx_gallery_patch.py
+++ docs/source/patch/_sphinx_gallery_patch.py
@@ -1,4 +1,4 @@
 from .sphinx_gallery_gen_rst import *
 from .sphinx_gallery_binder import *
 from .sphinx_gallery_scrapers import *
-#from .sphinx_gallery_docs_resolv import *
+from .sphinx_gallery_docs_resolv import *
