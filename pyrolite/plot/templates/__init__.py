bb65195cda6d80734dd28d638cc710c96cfbec83
Morgan Williams <morgan.j.williams@hotmail.com>
2020-11-13 11:55:18 +0800

    Note for plot.templates TODO for new matplotlib feature

--- pyrolite/plot/templates/__init__.py
+++ pyrolite/plot/templates/__init__.py
@@ -1,5 +1,9 @@
 """
 A utility submodule for standardised plot templates to be added to matplotlib axes.
+
+Todo
+----
+* Make use of new ax.axline features (https://matplotlib.org/3.3.1/users/whats_new.html#new-axes-axline-method)
 """
 
 from .pearce import pearceThNbYb, pearceTiNbYb
