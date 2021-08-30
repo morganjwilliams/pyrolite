84101b50a54ef3143cce90787ab572bbc2ee047c
Morgan Williams <morgan.williams@csiro.au>
2021-08-30 16:37:29 +0800

    Expand unit tests for CIPW and related functions

--- test/mineral/mineral_normative.py
+++ test/mineral/mineral_normative.py
@@ -1,7 +1,15 @@
 import unittest
 import pandas as pd
 import numpy as np
-from pyrolite.mineral.normative import unmix, endmember_decompose, CIPW_norm
+from pyrolite.mineral.normative import (
+    unmix,
+    endmember_decompose,
+    CIPW_norm,
+    _aggregate_components,
+    _update_molecular_masses,
+    LeMaitre_Fe_correction,
+    LeMaitreOxRatio,
+)
 from pyrolite.util.synthetic import normal_frame
 
 
@@ -57,19 +65,107 @@ class TestEndmemberDecompose(unittest.TestCase):
                 s = endmember_decompose(self.df, molecular=molecular)
 
 
+class Test_AggregateComponents(unittest.TestCase):
+    def setUp(self):
+        pass
+
+
+class Test_UpdateMolecularMasses(unittest.TestCase):
+    def setUp(self):
+        pass
+
+
+class TestLeMaitreFeCorrection(unittest.TestCase):
+    """
+    This function uses new updates to convert_chemistry to allow
+    acceptance of paired arrays for iron speciation.
+    """
+
+    def setUp(self):
+        self.df = normal_frame(columns=["SiO2", "Fe2O3", "FeO", "MnO"])
+        self.handler = "pyrolite.mineral.normative"
+
+    def test_default(self):
+        output = LeMaitre_Fe_correction(self.df)
+        self.assertIsInstance(output, pd.DataFrame)
+        self.assertTrue(all(output.columns == ["FeO", "Fe2O3"]))
+        # from a synthetic dataset, no data should be missing or zero
+        self.assertTrue(
+            (~np.isfinite(output.values) | np.isclose(output.values, 0)).sum() == 0
+        )
+
+    def test_modes(self):
+        pass
+
+    def test_from_FeO(self):
+        pass
+
+    def test_from_FeOT(self):
+        pass
+
+
+class TestLeMatireOxRatio(unittest.TestCase):
+    def setUp(self):
+        self.df = normal_frame(
+            columns=["SiO2", "TiO2", "Al2O3", "Fe2O3", "FeO", "MnO", "Na2O", "K2O"]
+        )
+        self.handler = "pyrolite.mineral.normative"
+
+    def test_default(self):
+        # should default to volcanic
+        ratio = LeMaitreOxRatio(self.df)
+        self.assertTrue(isinstance(ratio, pd.Series))
+        self.assertEqual(ratio.name, "FeO/(FeO+Fe2O3)")
+        self.assertTrue(
+            np.isclose(ratio, LeMaitreOxRatio(self.df, mode="volcanic")).all()
+        )
+
+    def test_modes(self):
+        """
+        Check that the two modes work as expected.
+        """
+        valid_modes = ["Volcanic", "Plutonic"]
+        # string should be normalised for comparison
+        # only the start of the string is required
+        valid_modes += (
+            [m.lower() for m in valid_modes]
+            + [m.upper() for m in valid_modes]
+            + [m[:4] for m in valid_modes]
+        )
+        valid_modes += [None]  # will default to Volcanic
+        for mode in valid_modes:
+            with self.subTest(mode=mode):
+                df = self.df
+                with self.assertLogs(self.handler, level="DEBUG") as cm:
+                    ratio = LeMaitreOxRatio(df, mode=mode)
+
+    def test_missing_columns(self):
+        """
+        The function should warn where required columns are missing.
+
+        For silica this is expected to fail; for the other components if they're
+        missing the difference might be less infuencial so for the moment it will
+        use reindexing and pass.
+        """
+        for missing in [["K2O"], ["K2O", "Na2O"]]:
+            with self.subTest(missing=missing):
+                df = self.df.drop(columns=missing)
+                with self.assertLogs(self.handler, level="WARNING") as cm:
+                    ratio = LeMaitreOxRatio(df)
+
+
 class TestCIPW(unittest.TestCase):
     def setUp(self):
         self.df = normal_frame(
             columns=["SiO2", "TiO2", "Al2O3", "Fe2O3", "FeO", "MnO"]
             + ["MgO", "CaO", "Na2O", "K2O", "P2O5", "CO2", "SO3"]
-            + []
         )
         self.handler = "pyrolite.mineral.normative"
 
     def test_default(self):
         norm = CIPW_norm(self.df)
 
-    def noncritical_missing():
+    def noncritical_missing(self):
         # should logger.debug mentioning those missing
         for drop in (["CO2"], ["CO2", "SO3"]):
             with self.subTest(drop=drop):
@@ -77,10 +173,19 @@ class TestCIPW(unittest.TestCase):
                     norm = CIPW_norm(self.df.drop(columns=drop))
                     logging_output = cm.output
 
-    def critical_missing():
+    def critical_missing(self):
         # should logger.warning mentioning the critical ones missing
         for drop in (["SiO2"], ["TiO2", "SO3"], ["SiO2", "MgO"]):
             with self.subTest(drop=drop):
                 with self.assertLogs(self.handler, level="WARNING") as cm:
                     norm = CIPW_norm(self.df.drop(columns=drop))
                     logging_output = cm.output
+
+    def test_adjust_all_fe(self):
+        pass
+
+    def test_fe_correction(self):
+        pass
+
+    def test_fe_correction_mode(self):
+        pass
