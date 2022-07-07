d203e1063f9aa201779e2668a4bd4397018412a6
Morgan Williams <morgan.williams@csiro.au>
2022-07-07 10:39:38 +0800

    Add Explicit Python 3.10 support for next version

--- setup.py
+++ setup.py
@@ -43,6 +43,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Framework :: Matplotlib",
         "Topic :: Scientific/Engineering",
         "Topic :: Software Development :: Libraries :: Python Modules",
