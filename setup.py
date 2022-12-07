d772038c96be13a3ec0ae4bd08b182861048ef11
Morgan Williams <morgan.williams@csiro.au>
2022-12-07 11:41:53 +0800

    Update to use mpltern>=v0.4 (allows matplotlib>=v3.6)

--- setup.py
+++ setup.py
@@ -56,8 +56,8 @@ setup(
         "typing-extensions",  # required for newer tinydb versions?
         "psutil",
         "periodictable",
-        "matplotlib<3.6",  # tying updates between matplotlib and mpltern
-        "mpltern",
+        "matplotlib", 
+        "mpltern>=0.4.0",
         "scipy>=1.2",  # uses scipy.optimize.Bounds, added around 1.2
         "mpmath",
         "sympy>=1.7",
