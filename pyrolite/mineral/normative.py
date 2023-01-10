c6fa45dd46b7be1611c21b4eeebd74b940e9155f
Tom Buckle <trb.buckle@gmail.com>
2023-01-10 12:47:55 +0000

    dropped columns from FREE df that are not required

--- pyrolite/mineral/normative.py
+++ pyrolite/mineral/normative.py
@@ -1214,6 +1214,8 @@ def CIPW_norm(
         ["FREEO_12b", "FREEO_12c", "FREEO_13", "FREEO_14", "FREEO_16"]
     ].sum(axis=1)
 
+    FREE.drop(["FREEO_12b", "FREEO_12c", "FREEO_13", "FREEO_14", "FREEO_16"], axis=1, inplace=True)
+
     ############################################################################
     # get masses of free components
     ############################################################################
