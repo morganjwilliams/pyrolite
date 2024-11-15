345ce92ef3a87ec8351429ada73f11e970f49dc7
Morgan Williams <morgan.williams@csiro.au>
2024-11-15 14:42:18 +1100

    Update example for interactive plotting API

--- pyrolite/util/plot/backend.py
+++ pyrolite/util/plot/backend.py
@@ -9,6 +9,11 @@ logger = Handle(__name__)
 
 BACKEND = "matplotlib"
 
+
+def _get_backend():
+    return BACKEND
+
+
 _backend_accessors = {"matplotlib": pyroplot_matplotlib}
 try:
     import plotly.graph_objects as go
@@ -27,14 +32,47 @@ def set_plotting_backend(backend=None, revert=False):
     if backend is not None:
         if backend in ["matplotlib", "plotly"]:
             global BACKEND
-            logger.debug(
-                "{} plotting backend to {}".format(
-                    "Reverting" if revert else "Setting", backend
+            if BACKEND != backend:
+                logger.debug(
+                    "{} plotting backend to {}".format(
+                        "Reverting" if revert else "Setting", backend
+                    )
                 )
-            )
-            BACKEND = backend
+                BACKEND = backend
         else:
             raise NotImplementedError("Backend {} not available.")
+
+
+class pyroplot(object):
+    def __init__(self, obj):
+        """
+        Custom dataframe accessor for pyrolite plotting.
+
+        Notes
+        -----
+            This accessor enables the coexistence of array-based plotting functions and
+            methods for pandas objects. This enables some separation of concerns.
+        """
+        self._obj = obj
+
+    def scatter(self, *args, backend=None, **kwargs):
+        with Backend(backend=backend):
+            return _backend_accessors[_get_backend()](self._obj).scatter(
+                *args, **kwargs
+            )
+
+    def spider(self, *args, backend=None, **kwargs):
+        with Backend(backend=backend):
+            return _backend_accessors[_get_backend()](self._obj).spider(*args, **kwargs)
+
+
+with warnings.catch_warnings():
+    # can get invalid values which raise RuntimeWarnings
+    # consider changing to np.errstate
+    warnings.simplefilter("ignore", category=UserWarning)
+    pd.api.extensions.register_dataframe_accessor("pyroplot")(
+        pyroplot  # _backend_accessors.get(self.backend)
+    )
 
 
 class Backend:
@@ -43,16 +81,9 @@ class Backend:
     """
 
     def __init__(self, backend=None):
-        self.start = BACKEND
+        self.start = _get_backend()
         self.backend = backend
         set_plotting_backend(self.backend)
-        with warnings.catch_warnings():
-            # can get invalid values which raise RuntimeWarnings
-            # consider changing to np.errstate
-            warnings.simplefilter("ignore", category=UserWarning)
-            pd.api.extensions.register_dataframe_accessor("pyroplot")(
-                _backend_accessors.get(self.backend)
-            )
 
     def __enter__(self):
         return self
