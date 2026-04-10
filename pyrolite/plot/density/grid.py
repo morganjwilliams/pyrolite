49838395eeae2f07d52d0a672fe78d9fbc0652d9
Morgan Williams <morgan.williams@csiro.au>
2026-04-10 16:00:04 +1000

    Bugfix - ordering in hex extent

--- pyrolite/plot/density/grid.py
+++ pyrolite/plot/density/grid.py
@@ -142,12 +142,12 @@ class DensityGrid(object):
             xex = [np.log(self.xmin / self.xstep), np.log(self.xmax * self.xstep)]
         else:
             xex = [self.xmin - self.xstep, self.xmax + self.xstep]
-
+        print(self.ystep, self.ymin, self.ymax)
         if self.logy:
             yex = [np.log(self.ymin / self.ystep), np.log(self.ymax * self.ystep)]
         else:
             yex = [self.ymin - self.ystep, self.ymax + self.ystep]
-        return xex + yex
+        return sorted(list(xex)) + sorted(list(yex))
 
     def kdefrom(
         self,
