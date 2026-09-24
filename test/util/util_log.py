b2e2be9744a5f9f27aed1c26641e515b23e0861d
Morgan Williams <morgan.williams@csiro.au>
2026-09-24 15:18:04 +1000

    Remove unused util.log.stream_log

--- test/util/util_log.py
+++ test/util/util_log.py
@@ -4,7 +4,7 @@ import unittest
 from io import StringIO
 
 import pyrolite.plot  # for logging checks  # noqa: F401
-from pyrolite.util.log import Handle, ToLogger, stream_log
+from pyrolite.util.log import Handle, ToLogger
 
 
 class Capturing(list):
@@ -17,51 +17,6 @@ class Capturing(list):
         self.extend(self._stringio.getvalue().splitlines())
         del self._stringio  # free up some memory
         sys.stdout = self._stdout
-
-
-class TestStreamLog(unittest.TestCase):
-    def setUp(self):
-        self.key = "pyrolite.plot"
-
-    def test_default(self):
-        for src in [self.key, None, logging.getLogger(__name__)]:
-            with self.subTest(src=src):
-                logger = stream_log(src)
-                # check that the logger has a single stream handler
-                handlers = [
-                    i for i in logger.handlers if isinstance(i, logging.StreamHandler)
-                ]
-                if src is not None:
-                    self.assertTrue(len(handlers) == 1)
-
-    def test_INFO(self):
-        level = "INFO"
-        logger = stream_log(self.key, level=level)
-        # check that the logger has a single stream handler
-        handlers = [i for i in logger.handlers if isinstance(i, logging.StreamHandler)]
-        self.assertTrue(len(handlers) == 1)
-        self.assertTrue(handlers[0].level == getattr(logging, level))
-
-    def test_WARNING(self):
-        level = "WARNING"
-        logger = stream_log(self.key, level=level)
-        # check that the logger has a single stream handler
-        handlers = [i for i in logger.handlers if isinstance(i, logging.StreamHandler)]
-        self.assertTrue(len(handlers) == 1)
-        self.assertTrue(handlers[0].level == getattr(logging, level))
-
-    def test_ouptut(self):
-        level = "WARNING"
-        logger = stream_log(self.key, level=level)
-        logger.warning("Test Warning")
-
-    def test_capture(self):
-        # set up the stream
-        with Capturing() as output:
-            pass
-            # trigger something with a logging output
-
-        # check that the relevant logging output has been redirected to stdout
 
 
 class TestHandle(unittest.TestCase):
